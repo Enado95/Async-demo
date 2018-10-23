6b4b654b7135f8f4dbd6221b8fc6819531d7729d
Brissett <brissettor@jncb.com>
2018-10-23 13:22:29 -0500

    Complete exercise

--- exercise.js
+++ exercise.js
@@ -1,35 +1,60 @@
 
-getCustomer(1, (customer) => {
-    console.log('Customer: ', customer);
-    if (customer.isGold) {
-      getTopMovies((movies) => {
-        console.log('Top movies: ', movies);
-        sendEmail(customer.email, movies, () => {
-          console.log('Email sent...')
-        });
-      });
-    }
-  });
+// getCustomer(1, (customer) => {
+//     console.log('Customer: ', customer);
+//     if (customer.isGold) {
+//       getTopMovies((movies) => {
+//         console.log('Top movies: ', movies);
+//         sendEmail(customer.email, movies, () => {
+//           console.log('Email sent...')
+//         });
+//       });
+//     }
+//   });
+
+  async function emailCustomer(id){
+      try{
+            const customer = await getCustomer(1);  //get customer details
+            console.log('Customer: ', customer);    //display details
+            if(customer.isGold){                    // check if customer is a gold member
+                const movies = await getTopMovies();// get movies
+                console.log('Top movies: ', movies);
+                const email = await sendEmail(customer.email, movies) // send to email from customer object and list of movies
+                console.log('Email sent..');
+            }
+            
+      }
+      catch(err){
+            console.log('Error', err.message);
+      }
+  }
+  emailCustomer();
   
-  function getCustomer(id, callback) {
-    setTimeout(() => {
-      callback({ 
-        id: 1, 
-        name: 'Mosh Hamedani', 
-        isGold: true, 
-        email: 'email' 
-      });
-    }, 4000);  
+  function getCustomer(id) {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({ 
+              id: 1, 
+              name: 'Mosh Hamedani', 
+              isGold: true, 
+              email: 'email' 
+            });
+          }, 4000);
+      });   
   }
   
-  function getTopMovies(callback) {
-    setTimeout(() => {
-      callback(['movie1', 'movie2']);
-    }, 4000);
+  function getTopMovies() {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(['movie1', 'movie2']);
+          }, 4000); 
+      });
   }
   
-  function sendEmail(email, movies, callback) {
-    setTimeout(() => {
-      callback();
-    }, 4000);
+  function sendEmail(email, movies, ) {
+      return new Promise ((resolve, reject) => {
+        setTimeout(() => {
+            resolve();
+          }, 4000);
+      });
+    
   }
