4ee9be427c9c777a226d40f666dfbd2e4ff46c5b
Brissett <brissettor@jncb.com>
2018-10-23 12:49:01 -0500

    Async await integration

--- index.js
+++ index.js
@@ -8,11 +8,26 @@ console.log('Before');
 // });
 console.log('After');
 
-getUser(1)
-    .then(user => getRepositories(user.gitHubUsername))
-    .then(repos => getCommits(repos[0]))
-    .then(commits => console.log('Commits', commits))
-    .catch(err => console.log('Error', err.message))
+// getUser(1)
+//     .then(user => getRepositories(user.gitHubUsername))
+//     .then(repos => getCommits(repos[0]))
+//     .then(commits => console.log('Commits', commits))
+//     .catch(err => console.log('Error', err.message));
+
+async function displayCommits() {
+    try{
+        const user = await getUser(1);
+        const repos = await getRepositories(user.gitHubUsername);
+        const commits = await getCommits(repos[0]);
+        console.log(commits);
+    }
+    catch(err)
+    {
+        console.log('Error', err.message);
+    }
+  
+}
+displayCommits();
 
 function getUser(id) {
     return new Promise((resolve, reject) => {
@@ -27,8 +42,8 @@ function getUser(id) {
 function getRepositories(username) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            console.log('Getting repos from gitHub...')
-            resolve([{'name':'project1'},{'name':'project2'},{'name':'project3'}]);
+            // resolve([{'name':'project1'},{'name':'project2'},{'name':'project3'}]);
+            reject(new Error('Could not get the repos'));
         }, 2000);
     })
 }
